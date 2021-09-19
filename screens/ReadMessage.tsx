98436f6c1f670519bbfc732b45e78be2839b39aa
jackie-z528 <jackie.zhou0528@gmail.com>
2021-09-19 08:59:31 -0400

    fixed rendering

--- screens/ReadMessage.tsx
+++ screens/ReadMessage.tsx
@@ -56,9 +56,10 @@ const [items, setItems] = React.useState([
         defaultValue=""
       />
       <View style={styles.cardContainer}>
-        allBottles.forEach(bottle => 
+        {allBottles.forEach(bottle => return (
           <BottleMessage value={value} style={styles.card} title='Geese' tags={['Animals']} details=''/>
-        );
+        );)}
+        <BottleMessage value={value} style={styles.card} title='Geese' tags={['Animals']} details=''/>
         <BottleMessage value={value} style={styles.card} title='Chungchun Rice Dogs' tags={['Food', 'Feeling Good']} details='A new rice dog place opened up in the University Plaza! It tastes amazing and I cant wait to go again! 10/10 would recommend.'/>
         <BottleMessage value={value} style={styles.card} title='Geese' tags={['Animals']} details='I saw geese crossing the street'/>
       </View>
